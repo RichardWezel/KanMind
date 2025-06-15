a7add4e02c1f41c23cc7dafdb6bdfeb9b5c85200
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-06-15 16:36:09 +0200

    created admin and path to auth in urls.py of projekt

--- Backend/KanMind_project/settings.py
+++ Backend/KanMind_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'auth_app',
+    'boards_app',
+    'tasks_app',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
