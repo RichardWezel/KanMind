49c1a1afdc6a587003b7d3b0391de7fc1bf18722
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-06-15 16:44:21 +0200

    created url paths for registration and login

--- Backend/KanMind_project/urls.py
+++ Backend/KanMind_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from auth_app.api.views import RegistrationView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth', include('auth_app.api.urls')),
+    path('api/registration/', RegistrationView.as_view(), name='registration'),
+    path('api/login', LoginView.as_view(), name='login'),
 ]
