73d7c6c17e346127a6715e68daa3a1a7824715cd
RichyClean <149680604+RichyClean@users.noreply.github.com>
2025-06-16 19:56:20 +0200

    admin settings set on CustomUser

--- Backend/auth_app/admin.py
+++ Backend/auth_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'fullname', 'is_staff', 'is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Zusätzliche Felder', {'fields': ('fullname',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Zusätzliche Felder', {'fields': ('fullname',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
